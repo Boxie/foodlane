34ae91eee3947915900f7e39493614ee3cf153d9
jonasgrunert <jonas@grunert-berlin.de>
2017-05-03 15:33:11 +0200

    Implementing latest Commit
    Added everything needed to start on implementing sweet alert

--- app/config/initializers/server.js
+++ app/config/initializers/server.js
@@ -26,6 +26,10 @@ var start =  function(cb) {
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({ extended: false }));
 
+    //set Paths
+    app.set("views", path.join(__dirname, "../../views"));
+    app.use(express.static(path.join(__dirname, '../../public')));
+
     logger.info('[SERVER] Initializing passport');
     require('../pass')(app, passport);
 
@@ -37,9 +41,6 @@ var start =  function(cb) {
     // Set PugJS as view engine
     app.set('view engine', 'pug');
 
-    //set Paths
-    app.set("views", path.join(__dirname, "../../views"));
-    app.use(express.static(path.join(__dirname, '../../public')));
 
     app.listen(serverConfig.get('NODE_PORT'));
     logger.info('[SERVER] Listening on port ' + serverConfig.get('NODE_PORT'));
